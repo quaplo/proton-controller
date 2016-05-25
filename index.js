0e1fa34f50f7dbd30e7aea7fb9a061b447906e93
Gregor Demo <demo.gregor@gmail.com>
2016-05-25 22:35:11 +0200

    pure restify
    pure restify instalation

--- index.js
+++ index.js
@@ -1,41 +1,14 @@
-var WebSocketServer = require("ws").Server
-var http = require("http")
-var express = require("express")
-var app = express()
-var port = process.env.PORT || 5000
+var restify = require('restify');
 
-app.use(express.static(__dirname + "/"))
+function respond(req, res, next) {
+  res.send('hello ' + req.params.name);
+  next();
+}
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+var server = restify.createServer();
+server.get('/hello/:name', respond);
+server.head('/hello/:name', respond);
+
+server.listen(8080, function() {
+  console.log('%s listening at %s', server.name, server.url);
 });
-
-app.get('/c', function(req, res){
-  res.sendFile(__dirname + '/input.html');
-});
-
-
-var server = http.createServer(app)
-server.listen(port)
-
-console.log("http server listening on %d", port)
-
-var wss = new WebSocketServer({server: server})
-console.log("websocket server created")
-
-
-
-wss.on("connection", function(ws) {
-
-  ws.on('message', function(data, flags) {
-    wss.clients.forEach(function each(client) {
-      client.send(data);
-    });
-
-  });
-
-
-  ws.on("close", function() {
-    console.log("websocket connection close")
-  })
-})
